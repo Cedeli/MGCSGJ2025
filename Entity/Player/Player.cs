91ce08d99b70315f40e6d7874e7f1b12d63fc83b
OverLuked <edillorluke@gmail.com>
2025-04-08 15:01:03 +0800

    Port character model into useable scene

--- Entity/Player/Player.cs
+++ Entity/Player/Player.cs
@@ -1,6 +1,14 @@
 using Godot;
 using System;
 
-public partial class Player : Node
+public partial class Player : CharacterBody3D
 {
+    // Player stats set this into a global stat sheet if there are multiple player models
+    [Export] public float Speed { get; set; } = 35.5f;
+    [Export] public float Mass { get; set; } = 75f;
+
+    public override void _Ready()
+    {
+        GD.Print("Player Ready!");
+    }
 }
