8891debf0019b7714fc2c2bc3497ae31c5aef5f9
OverLuked <edillorluke@gmail.com>
2025-04-08 15:01:19 +0800

    Initialize player controller

---
+++ Entity/Player/PlayerController.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class PlayerController : Node3D
+{
+    [Export] private Player _player;
+
+    private Vector3 _velocity = Vector3.Zero;
+
+    public override void _Ready()
+    {
+        GD.Print("Player Controller Ready");
+    }
+    
+}
