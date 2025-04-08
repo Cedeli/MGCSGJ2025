22bd95c3e75fe87f3c23d741cef405d82a3adbef
OverLuked <edillorluke@gmail.com>
2025-04-08 15:07:35 +0800

    Add PlayerController into Player

--- Entity/Player/PlayerController.cs
+++ Entity/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public partial class PlayerController : Node3D
     [Export] private Player _player;
 
     private Vector3 _velocity = Vector3.Zero;
+    private Vector3 _direction = Vector3.Zero;
 
     public override void _Ready()
     {
