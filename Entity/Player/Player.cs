5384fc074bed24d7a0ca53fc513dd234f06bf2cb
OverLuked <edillorluke@gmail.com>
2025-04-09 05:19:40 +0800

    Fix typo

--- Entity/Player/Player.cs
+++ Entity/Player/Player.cs
@@ -6,7 +6,7 @@ public partial class Player : RigidBody3D
     // Player stats set this into a global stat sheet if there are multiple player models
     [ExportCategory("Stats")]
     [Export] private float _speed { get; set; } = 35.5f;
-    [Export] private float _thurst = 1.0f;
+    [Export] private float _thrust = 1.0f;
     [Export] private float _orientSpeed = 0.5f;
 
     // For Aiming
@@ -30,7 +30,7 @@ public partial class Player : RigidBody3D
     public override void _Ready()
     {
         GD.Print("Player Ready!");
-        _controller.SetPlayer(this, _speed, Mass, _thurst, _orientSpeed, _mouseSens, _mouseDelta, IsGrounded,
+        _controller.SetPlayer(this, _speed, Mass, _thrust, _orientSpeed, _mouseSens, _mouseDelta, IsGrounded,
             Ground);
     }
 }
