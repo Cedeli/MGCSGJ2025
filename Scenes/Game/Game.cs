9fb11eb3535c2222c77bdf256cbadee637f7b029
kyleconciso <kyleconciso@gmail.com>
2025-04-09 20:47:19 +0800

    Unhide CelestialBody instances

--- Scenes/Game/Game.cs
+++ Scenes/Game/Game.cs
@@ -2,38 +2,38 @@ using Godot;
 
 public partial class Game : Node3D
 {
-    private GameManager _gameManager;
-    private const string PAUSE_SCENE_PATH = "res://Scenes/Pause/Pause.tscn";
+	private GameManager _gameManager;
+	private const string PAUSE_SCENE_PATH = "res://Scenes/Pause/Pause.tscn";
 
-    public override void _Ready()
-    {
-        _gameManager = GetNode<GameManager>("/root/GameManager");
-        _gameManager.PushScene("res://Scenes/HUD/HUD.tscn");
-        _gameManager.SceneChanged += OnSceneChanged;
-    }
+	public override void _Ready()
+	{
+		_gameManager = GetNode<GameManager>("/root/GameManager");
+		_gameManager.PushScene("res://Scenes/HUD/HUD.tscn");
+		_gameManager.SceneChanged += OnSceneChanged;
+	}
 
-    private void OnSceneChanged(string scenePath, bool isPushing)
-    {
-        if (scenePath == PAUSE_SCENE_PATH)
-        {
-            if (isPushing)
-            {
-                // pause game when pause screen is pushed
-                ProcessMode = ProcessModeEnum.Disabled;
-            }
-            else
-            {
-                // resume game when pause screen is popped
-                ProcessMode = ProcessModeEnum.Inherit;
-            }
-        }
-    }
+	private void OnSceneChanged(string scenePath, bool isPushing)
+	{
+		if (scenePath == PAUSE_SCENE_PATH)
+		{
+			if (isPushing)
+			{
+				// pause game when pause screen is pushed
+				ProcessMode = ProcessModeEnum.Disabled;
+			}
+			else
+			{
+				// resume game when pause screen is popped
+				ProcessMode = ProcessModeEnum.Inherit;
+			}
+		}
+	}
 
-    public override void _ExitTree()
-    {
-        if (_gameManager != null)
-        {
-            _gameManager.SceneChanged -= OnSceneChanged;
-        }
-    }
+	public override void _ExitTree()
+	{
+		if (_gameManager != null)
+		{
+			_gameManager.SceneChanged -= OnSceneChanged;
+		}
+	}
 }
