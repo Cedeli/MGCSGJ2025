0889be2f0e6b47dc5966bf7e1d00d1ad7cfac1b2
Cedeli <96904745+Cedeli@users.noreply.github.com>
2025-04-11 20:52:09 +0800

    Add no ammo drain during powerup duration

--- Entity/Item/PowerupItem/PowerupItem.cs
+++ Entity/Item/PowerupItem/PowerupItem.cs
@@ -3,30 +3,24 @@ using Godot;
 
 public enum PowerupType
 {
-	FireRate,
-	BulletDamage,
-	BulletSpeed,
+    FireRate,
+    BulletDamage,
+    BulletSpeed,
 }
 
 public partial class PowerupItem : Item
 {
-	[Export]
-	public PowerupType EffectType = PowerupType.FireRate;
+    [Export] public PowerupType EffectType = PowerupType.FireRate;
 
-	[Export(PropertyHint.Range, "0.1, 3.0, 0.1")]
-	public float Multiplier = 1.5f;
+    [Export(PropertyHint.Range, "0.1, 3.0, 0.1")]
+    public float Multiplier = 1.5f;
 
-	[Export(PropertyHint.Range, "1.0, 30.0, 1.0")]
-	public float Duration = 10.0f;
+    [Export(PropertyHint.Range, "1.0, 30.0, 1.0")]
+    public float Duration = 10.0f;
 
-	protected override bool ApplyEffect(Player player)
-	{
-		player?.ApplyGunPowerup(EffectType, Multiplier, Duration);
-		return true;
-	}
-
-	public override void _Ready()
-	{
-		base._Ready();
-	}
+    protected override bool ApplyEffect(Player player)
+    {
+        player?.ApplyGunPowerup(EffectType, Multiplier, Duration);
+        return true;
+    }
 }
